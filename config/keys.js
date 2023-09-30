48d0be13d18f6ea9b3043f35e2cc17efb219d4b4
John Blesson-Rowe <106766947+Johnblesson@users.noreply.github.com>
2023-09-30 19:39:42 +0000

    express auth with passport

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+// const { mongo } = require("mongoose");
+
+module.exports = {
+    MONGO_URI: 'mongodb://0.0.0.0:27017/passport'
+}
---
+++ database/db.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const db = require('../config/keys').MONGO_URI;
+
+// Connect to MongoDB
+mongoose
+  .connect(
+    db,
+    { useNewUrlParser: true ,useUnifiedTopology: true}
+  )
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log(err));
